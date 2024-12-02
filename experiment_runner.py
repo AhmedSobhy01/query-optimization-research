4519cb642f73c31f541a368d1e1e387471175a8b
AhmedSobhy01 <samahmedsobhy@gmail.com>
2024-12-02 15:51:44 +0200

    feat: add database maintenance functions and increase runs per query

--- experiment_runner.py
+++ experiment_runner.py
@@ -16,14 +16,50 @@ DB_PARAMS = {
     "port": 5444,
 }
 
-RUNS_PER_QUERY = 25
+RUNS_PER_QUERY = 35
 RESULTS_FILE = "experiment_results.csv"
 
 POSTGRES_BIN_PATH = r"C:\Program Files\PostgreSQL\17\bin"
 POSTGRES_DATA_PATH = r"C:\Program Files\PostgreSQL\17\data"
 
 
-def execute_query(conn, query):
+def vacuum_database():
+    conn = psycopg2.connect(**DB_PARAMS)
+    conn.set_session(autocommit=True)
+    with conn.cursor() as cur:
+        cur.execute("VACUUM FULL;")
+    conn.commit()
+    conn.close()
+
+
+def restart_postgres():
+    if os.name == "nt":
+        subprocess.run(
+            f'"{POSTGRES_BIN_PATH}\\pg_ctl.exe" restart -D "{POSTGRES_DATA_PATH}"',
+            shell=True,
+            check=True,
+        )
+    else:
+        os.system("sudo systemctl restart postgresql")
+
+
+def clear_cache():
+    with psycopg2.connect(**DB_PARAMS) as conn:
+        conn.set_session(autocommit=True)
+        with conn.cursor() as cur:
+            cur.execute("SELECT pg_stat_reset();")
+            cur.execute("DISCARD ALL;")
+        conn.commit()
+
+    if os.name != "nt":
+        os.system("sudo sync && sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'")
+        print("System cache cleared.")
+
+
+def execute_query(conn, query, clear_caches=True):
+    if clear_caches:
+        clear_cache()
+
     with conn.cursor(cursor_factory=DictCursor) as cur:
         start_time = time.time()
         cur.execute(query)
@@ -35,6 +71,8 @@ def execute_query(conn, query):
 
 def run_experiments():
     file_exists = os.path.isfile(RESULTS_FILE)
+
+    vacuum_database()
 
     with open(RESULTS_FILE, "a", newline="") as csvfile:
         fieldnames = [
@@ -92,14 +130,7 @@ def apply_configuration(config_commands):
     conn.commit()
     conn.close()
 
-    if os.name == "nt":
-        subprocess.run(
-            f'"{POSTGRES_BIN_PATH}\\pg_ctl.exe" restart -D "{POSTGRES_DATA_PATH}"',
-            shell=True,
-            check=True,
-        )
-    else:
-        os.system("sudo systemctl restart postgresql")
+    restart_postgres()
 
 
 def reset_indexes():
