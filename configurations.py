5a962d1d41aa8b99896e571b9d9fab02c7408552
Habiba Ayman <137416623+habibayman@users.noreply.github.com>
2024-12-06 15:48:09 +0200

    Update configurations.py

--- configurations.py
+++ configurations.py
@@ -1,25 +1,16 @@
 configurations = {
     "Baseline": [
-        "ALTER SYSTEM SET work_mem = '4MB';",
-        "ALTER SYSTEM SET effective_cache_size = '4GB';",
-        "ALTER SYSTEM SET max_parallel_workers_per_gather = 2;",
-        "ALTER SYSTEM SET random_page_cost  = 4.0;",
-        "ALTER SYSTEM SET maintenance_work_mem = '64MB';",
-    ],
-    
-    "FirstArr": [
+        "ALTER SYSTEM SET shared_buffers = '1GB';",
         "ALTER SYSTEM SET work_mem = '16MB';",
-        "ALTER SYSTEM SET effective_cache_size = '8GB';",
+        "ALTER SYSTEM SET effective_cache_size = '4GB';",
+        "ALTER SYSTEM SET maintenance_work_mem = '512GB';",
         "ALTER SYSTEM SET max_parallel_workers_per_gather = 2;",
-        "ALTER SYSTEM SET random_page_cost  = 2.0;",
-        "ALTER SYSTEM SET maintenance_work_mem = '128MB';",
     ],
-    "SecondArr": [
-        "ALTER SYSTEM SET work_mem = '256MB';",
-        "ALTER SYSTEM SET effective_cache_size = '8GB';",
-        "ALTER SYSTEM SET max_parallel_workers_per_gather = 8;",
-        "ALTER SYSTEM SET random_page_cost  = 1.1;",
+    "High Memory": [
+        "ALTER SYSTEM SET shared_buffers = '2GB';",
+        "ALTER SYSTEM SET work_mem = '32MB';",
+        "ALTER SYSTEM SET effective_cache_size = '6GB';",
         "ALTER SYSTEM SET maintenance_work_mem = '1GB';",
+        "ALTER SYSTEM SET max_parallel_workers_per_gather = 4;",
     ],
-
 }
